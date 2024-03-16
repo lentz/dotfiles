c34b5e1b4d135985d66b24f0e5221919e6b8d504
Jesse Lentz <jesselentz@gmail.com>
2024-03-16 13:54:20 -0400

    Install fzf key bindings

--- install.sh
+++ install.sh
@@ -12,4 +12,7 @@ ln -sf ~/.dotfiles/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/aliases.sh ~/.aliases.sh
 ln -sf ~/.dotfiles/git-prompt.sh ~/.git-prompt.sh
 
+echo "Setup FZF shell integration"
+/opt/homebrew/opt/fzf/install --no-bash --no-fish --no-update-rc
+
 echo "Complete!"
