4925abf7b95321fe4e8fa69a2522edcded88d568
Jesse Lentz <jesselentz@gmail.com>
2024-05-12 22:21:49 -0400

    Update install for global npm packages

--- install.sh
+++ install.sh
@@ -15,4 +15,6 @@ ln -sf ~/.dotfiles/git-prompt.sh ~/.git-prompt.sh
 echo "Setup FZF shell integration"
 /opt/homebrew/opt/fzf/install --no-bash --no-fish --no-update-rc
 
+npm install --global @fsouza/prettierd
+
 echo "Complete!"
